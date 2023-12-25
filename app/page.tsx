0ded7390eba0a802a991f1b1c841d1de268dd0fe
dvhuzg <dohungdvh@gmail.com>
2023-12-25 14:08:10 +0700

    API + Car Card

--- app/page.tsx
+++ app/page.tsx
@@ -13,26 +13,26 @@ export default async function Home() {
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
-          <div className="home__filters">
-            <SearchBar />
-            <div className="home__filter-container">
-              <CustomFilter title="fuel" />
-              <CustomFilter title="year" />
-            </div>
+        </div>{" "}
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
-          {!isDataEmpty ? (
-            <section>
-              <div className="home__cars-wrapper">
-                {allCars?.map((car) => <CarCard car={car} />)}
-              </div>
-            </section>
-          ) : (
-            <div className="home__error-container">
-              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-              <p>{allCars?.message}</p>
-            </div>
-          )}
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car, index) => <CarCard car={car} key={index} />)}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- utils/index.ts
+++ utils/index.ts
@@ -9,3 +9,14 @@ export async function fetchCars() {
   );
   return await response.json();
 }
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50;
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
+
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  const rentalRatePerDat = basePricePerDay + mileageRate + ageRate;
+  return rentalRatePerDat.toFixed(0);
+};
